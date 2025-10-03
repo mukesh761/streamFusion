270967cb8535cefbd8c90ae93277f50b1d18cade
Mukesh Kumar <mukesh737462@gmail.com>
2025-10-03 19:44:34 +0530

    updated routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import SingUp from "./pages/SingUp";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Confress from "./pages/Confress";
+import { userContext } from "./context/User.context";
 
 const App=()=>{
+  const {islogin}=useContext(userContext)
   return(
     <Router>
       <Routes>
         
-        <Route path="/login" element={<Login/>} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/signup" element={<SingUp/>}/>
-         <Route path="/confress/:roomId" element={<Confress/>}/>
+        <Route path="/login" element={islogin?<Home/>:<Login/>} />
+        <Route path="/" element={islogin?<Home/>:<Login/>} />
+        <Route path="/signup" element={islogin?<Home/>:<SingUp/>}/>
+         <Route path="/confress/:roomId" element={islogin?<Confress/>:<Login/>}/>
       </Routes>
     </Router>
   )
